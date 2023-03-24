40dc0b3716b94f59401e642cf84c76d9065ff620
EmilioDiazR <st3832@utr.edu.mx>
2023-03-24 10:00:51 -0600

    Se agrego compomentes

---
+++ App_Styles.js
@@ -0,0 +1,142 @@
+const styles = ({
+
+    Base_Style: {
+      width: '100%',
+      padding: 30,
+      flex:1,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'stretch'
+    },
+  
+    Container_Style:{
+      backgroundColor: '#D9E0EF',
+      width: '100%',
+      height: '90%',
+      justifyContent: 'center',
+      alignItems: 'stretch',
+      flexDirection: 'column',
+      borderRadius: 15,
+    },
+  
+    topIcon_Style:{
+      width: 130,
+      height: 130,
+      backgroundColor: '#fff',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderWidth: 7,
+      borderColor: '#870D0D' ,
+      borderRadius: 100
+    },
+  
+    Top_Style:{
+      width: '100%',
+      height: '15%',
+      backgroundColor: '#090D36',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderTopLeftRadius: 15,
+      borderTopRightRadius: 15,
+      elevation: 20,
+     
+    },
+  
+    Id_Style:{
+      width: '100%',
+      height: '15%',
+      backgroundColor: '#F0F2F8',
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 15,
+    },
+  
+    Center_Style:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      padding: 35,
+      flex:1,
+      width: '100%',
+      height: '45%',
+      backgroundColor: '#D9E0EF',
+    },
+    
+    Elements_Style:{
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      flex:1,
+    },
+  
+    Down_Style: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexDirection: 'column',
+      padding: 2,
+      flex:1,
+      width: '100%',
+      height: '45%',
+      backgroundColor: '#D9E0EF',
+      borderBottomLeftRadius: 15,
+      borderBottomRightRadius: 15,
+    },
+  
+    Foot_Style:{
+      width: '50px',
+      height: '50px',
+      backgroundColor: '#fff',
+      borderRadius: 40,
+    },
+  
+    //Text
+  
+    Titles_Style:{
+      fontFamily: 'sans-serif',
+      fontSize: 20,
+      fontWeight: 'bold'
+  
+    },
+  
+    idText_Style:{
+      fontFamily: 'sans-serif',
+      fontSize: 5,
+      fontWeight: 'bold',
+      color: '#D9D9D9',
+    },
+    //icons
+  
+    logoIcon_Style: {
+      width: 115,
+      height: 115,
+      borderRadius: 100,
+      opacity: 0.5,
+    },
+  
+    downloadIcon_Style:{
+      width: 70,
+      height: 70,
+      borderRadius: 15,
+      borderWidth: 5,
+  
+    },
+  
+    //shadow
+  
+    elevation: {
+      
+      shadowColor: '#52006A',
+    },
+  
+    shadowProp: {
+      shadowColor: '#171717',
+      shadowOffset: {width: -2, height: 4},
+      shadowOpacity: 0.2,
+      shadowRadius: 3,
+    },
+  
+  
+  
+  });
+  
+  export default styles;
